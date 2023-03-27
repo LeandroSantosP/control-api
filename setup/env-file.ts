e214e191b7497f83f2653b51cd334407cf87956c
LeandroSantosP <skp.tridimensional@gmail.com>
2023-03-27 13:24:44 -0300

    fix: .env exmaple

--- setup/env-file.ts
+++ setup/env-file.ts
@@ -2,14 +2,14 @@ const fs = require('fs');
 const dotenv = require('dotenv');
 // get variable from example even.example
 const envExemploKeys = Object.keys(
-   dotenv.config({
-      path: '.env.example',
-   }).parsed
+  dotenv.config({
+    path: '.env.example',
+  }).parsed
 );
 
 /*  */
 const env = envExemploKeys.reduce((storage, currentKey) => {
-   return `${storage}${currentKey}=${process.env[currentKey]}\n`;
+  return `${storage}${currentKey}=${process.env[currentKey]}\n`;
 }, '');
 
 // this variable, search for values inside process.env
