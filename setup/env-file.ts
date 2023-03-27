eee0aba1c4ceea73dcbee2dd37724bdace6696e6
LeandroSantosP <skp.tridimensional@gmail.com>
2023-03-27 13:17:25 -0300

    get setup for .env

---
+++ setup/env-file.ts
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+// get variable from example even.example
+const envExemploKeys = Object.keys(
+   dotenv.config({
+      path: '.env.example',
+   }).parsed
+);
+
+/*  */
+const env = envExemploKeys.reduce((storage, currentKey) => {
+   return `${storage}${currentKey}=${process.env[currentKey]}\n`;
+}, '');
+
+// this variable, search for values inside process.env
+// created a new file .env with this variable e your values
+fs.writeFileSync('.env', env);
